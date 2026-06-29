d730f0df134d5301c23ed52f95d5e0f29df3a7a4
Saeed Johari <59645534+persgeek@users.noreply.github.com>
2026-06-29 14:11:29 +0330

    Add simple install file

---
+++ simple_install.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://raw.githubusercontent.com/caasify/hetzner_abuse_blocker/main/install.sh | bash -s -- --archive-url https://github.com/caasify/hetzner_abuse_blocker/archive/refs/tags/v1.0.0.tar.gz
